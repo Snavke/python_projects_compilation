f7df5e470f30b02fa0be2f9837d8b1a2c50b3f44
Snavke <altheo.mananquil@gmail.com>
2025-03-11 00:01:48 +0800

    First and Last Commit

---
+++ batch1_practice_programs/print_equal_number_equality_checker.py.py
@@ -0,0 +1,10 @@
+
+# Prompts user to enter first number and assigns it to a variable called "num1"
+num1 = float(input("Enter Number: "))
+# Prompts user to enter second number and assigns it to a variable called "num2"
+num2 = float(input("Enter Number: "))
+
+# Checks if numbers are equal
+if num1 == num2:
+    # Prints equal if condition is met
+    print ("Equal")
