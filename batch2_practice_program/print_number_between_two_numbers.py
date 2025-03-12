c692f6182accd3cf5efd6771cd246f449db73c38
Snavke <altheo.mananquil@gmail.com>
2025-03-13 02:45:15 +0800

    Psuedo Code Updated, Functional Code, Final Commit

--- batch2_practice_program/print_number_between_two_numbers.py
+++ batch2_practice_program/print_number_between_two_numbers.py
@@ -1,11 +1,10 @@
 # prompt user to initialize the first and last number
-
-# 
-
-# print numbers between the two numbers entered
-
 first_number = int(input("Enter Number: "))
 last_number = int(input("Enter Number: "))
+
+# for loop create range with start as the "first_number" and stop as "last_number" both entered by user
 for i in range(first_number, last_number):
+# skip first number in range since prompt only need the numbers in between
     if i != first_number:
+# print numbers between the two numbers entered
         print (i)
