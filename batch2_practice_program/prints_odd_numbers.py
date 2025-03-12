0d30e6eb7482371c14f17d6455ae48083d470eb8
Snavke <altheo.mananquil@gmail.com>
2025-03-13 02:06:24 +0800

    Pseudo Code Changes, Functional Code, Final Commit

--- batch2_practice_program/prints_odd_numbers.py
+++ batch2_practice_program/prints_odd_numbers.py
@@ -1,7 +1,10 @@
 # variable set to 1 to start 
-
+number_odd = 1
 # use while loop to print 1-100
-
-# filter out odd number by incrementing by 2
-
+while number_odd <= 100:
 # print odd numbers 
+    print (number_odd)
+# filter out odd number by incrementing by 2
+    number_odd += 2
+
+    
