1203de9e7a426a0b764ddd7ecebff57534868e34
Snavke <altheo.mananquil@gmail.com>
2025-03-13 03:16:46 +0800

    Attempted, no success, second commit

--- batch3_practice_program/display_numbers_no_duplicate.py
+++ batch3_practice_program/display_numbers_no_duplicate.py
@@ -3,3 +3,9 @@
 # make way to determine if number is duplicated
 
 # print numbers without duplicate
+
+
+for i in range (0,10):
+    no_duplicate = int(input("Enter Number: "))
+    if no_duplicate == i:
+        print (no_duplicate)
