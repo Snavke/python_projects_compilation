3dc4712b1f12d15b08f0183aeae50e84049b0fb4
Snavke <altheo.mananquil@gmail.com>
2025-03-13 01:49:31 +0800

    Pseudo Code, First Commit

---
+++ batch2_practice_program/even_number_counter.py
@@ -0,0 +1,10 @@
+# initiliaze where to store the amount of even numbers are in the input
+
+# loop for entering 10 numbers
+
+# condition to determine if number is even
+  
+# if condition is met, count as +1
+
+# print the amount of even numbers
+    
