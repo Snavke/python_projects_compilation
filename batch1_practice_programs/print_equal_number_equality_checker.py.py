0c8a8d4db59dff2739f59e920ff2fd1919088b82
Snavke <altheo.mananquil@gmail.com>
2025-03-13 01:14:12 +0800

    Variable & Pseudo Code Changes, Final Commit

--- batch1_practice_programs/print_equal_number_equality_checker.py.py
+++ batch1_practice_programs/print_equal_number_equality_checker.py.py
@@ -1,10 +1,9 @@
 
-# Prompts user to enter first number and assigns it to a variable called "num1"
-num1 = float(input("Enter Number: "))
-# Prompts user to enter second number and assigns it to a variable called "num2"
-num2 = float(input("Enter Number: "))
+# prompt user to initialize number
+first_number = float(input("Enter Number: "))
+# prompts user to initialize second number 
+second_number = float(input("Enter Number: "))
 
-# Checks if numbers are equal
-if num1 == num2:
-    # Prints equal if condition is met
+# check if numbers are equalif first_number == second_number:
+    # print equal if condition is met
     print ("Equal")
