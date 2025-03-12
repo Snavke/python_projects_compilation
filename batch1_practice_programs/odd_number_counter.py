83fa726c9c80565df34dab440849ec259cfbca67
Snavke <altheo.mananquil@gmail.com>
2025-03-13 01:35:58 +0800

    Variable & Pseudo Code Changes, Final Commit

--- batch1_practice_programs/odd_number_counter.py
+++ batch1_practice_programs/odd_number_counter.py
@@ -1,14 +1,14 @@
-# initiliazes where to store the amount of odd numbers are in the input
-count = 0
+# initiliaze where to store the amount of odd numbers are in the input
+count_of_odd = 0
 
 # loop for entering 10 numbers
 for i in range (0,10):
     num = float(input("Enter Number: "))
-    # condition for determining if number is odd
+    # condition to determine if number is odd
     if num % 2 != 0:
-        # if condition is met, increment by 1
-        count += 1
+        # if condition is met, count as +1
+        count_of_odd += 1
 
-# prints the amount of odd numbers
-print (count)
+# print the amount of odd numbers
+print (count_of_odd)
     
