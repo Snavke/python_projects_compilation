b1a143afd86f3751eb3670a954117dba2cca06eb
Snavke <altheo.mananquil@gmail.com>
2025-03-13 01:38:20 +0800

    Pseudo Code & Variable Changes, Final Commit

--- batch2_practice_program/print_not_equal_number_equality_checker.py
+++ batch2_practice_program/print_not_equal_number_equality_checker.py
@@ -1,9 +1,8 @@
 # prompts user to enter 2 numbers for comparison
-num1 = float(input("Enter Number: "))
-num2 = float(input("Enter Number: "))
+first_number = float(input("Enter Number: "))
+second_number = float(input("Enter Number: "))
 
-# checks if number aren't equal
-if num1 != num2:
-    # prints the word "Not Equal"
+# checks if number are not equal
+if first_number != second_number:
+    # print the word "Not Equal"
     print ("Not Equal")
-
