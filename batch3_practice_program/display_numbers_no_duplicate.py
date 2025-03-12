dae81f21870f6d84a23818b60957f6818de75e6e
Snavke <altheo.mananquil@gmail.com>
2025-03-13 03:23:05 +0800

    Scrapped first code draft, Pseudo Code Updates, 3rd Commit

--- batch3_practice_program/display_numbers_no_duplicate.py
+++ batch3_practice_program/display_numbers_no_duplicate.py
@@ -1,11 +1,7 @@
+# use lists to store inputs
+
 # prompt user to enter 10 inputs using for loop 
 
 # make way to determine if number is duplicated
 
 # print numbers without duplicate
-
-
-for i in range (0,10):
-    no_duplicate = int(input("Enter Number: "))
-    if no_duplicate == i:
-        print (no_duplicate)
