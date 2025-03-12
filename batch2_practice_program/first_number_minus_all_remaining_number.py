8e27fdb6fb440ee32fc075428ea7469735c79902
Snavke <altheo.mananquil@gmail.com>
2025-03-13 01:36:39 +0800

    Pseudo Code & Variable Changes, Final Commit

--- batch2_practice_program/first_number_minus_all_remaining_number.py
+++ batch2_practice_program/first_number_minus_all_remaining_number.py
@@ -1,10 +1,11 @@
 # asks user for the first number to subtract all of the other numbers
 first_number = float(input("Enter Number: "))
 
-# creates a prompt from 1 - 9 for the other 9 remaining numbers 
+# create a prompt from 1 - 9 for the other 9 remaining numbers 
 for i in range (0, 9):
     remaining_number = float(input("Enter Number: "))
+    # subtract first number from remaining number
     first_number -= remaining_number
 
-# prints the first number with the subtracted values of the remaining number
+# print the first number with the subtracted values of the remaining number
 print (first_number)
