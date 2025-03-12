811e82192a48cfec64ed3c7b41385832659e78f3
Snavke <altheo.mananquil@gmail.com>
2025-03-13 01:46:11 +0800

    Pseudo Code & Variable Changes, Final Commit

--- batch2_practice_program/prints_smaller_number.py
+++ batch2_practice_program/prints_smaller_number.py
@@ -1,11 +1,11 @@
-# prompts user to enter 2 numbers for comparison
-num1 = float(input("Enter Number: "))
-num2 = float(input("Enter Number: "))
+# prompt user to initialize the first and second number
+first_number = float(input("Enter Number: "))
+second_number = float(input("Enter Number: "))
 
-# compares the value of both values entered
-if num1 < num2:
-    # prints the lesser number if condition is met.
-    print (num1)
+# compare the value of both values entered
+if first_number < second_number:
+    # print the lesser number if condition is met
+    print (first_number)
 else: 
-    # prints the other number if condition is not met.
-    print (num2)
+    # print the other number if condition is not met
+    print (second_number)
