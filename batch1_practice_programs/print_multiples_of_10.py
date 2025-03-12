ab1812238dc0af45eb2fe1eec0ecf84ad759671d
Snavke <altheo.mananquil@gmail.com>
2025-03-13 01:19:34 +0800

    Pseudo Code Changes, Final Commit

--- batch1_practice_programs/print_multiples_of_10.py
+++ batch1_practice_programs/print_multiples_of_10.py
@@ -1,6 +1,6 @@
-# creates a range for 0-100 
+# create a range for 0-100 
 for i in range (0,101):
-    # filters out all multiples of 10 
+    # filter out all multiples of 10 
     if i % 10 == 0:
         # prints out all numbers ending in 0
         print (i)
