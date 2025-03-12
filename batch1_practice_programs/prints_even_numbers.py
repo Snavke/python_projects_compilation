2c47f6cf2e5c13f482498b990f5335280838a193
Snavke <altheo.mananquil@gmail.com>
2025-03-13 01:20:21 +0800

    Pseudo Code Changes, Final Commit

--- batch1_practice_programs/prints_even_numbers.py
+++ batch1_practice_programs/prints_even_numbers.py
@@ -1,6 +1,6 @@
 # creates a range from 0 - 100 
 for i in range (0,101):
-    # filters out even number by using modulos 2
+    # filters out even number by using modulus by 2
     if i % 2 == 0:
-        # prints even numbers
+        # print even numbers
         print (i)
