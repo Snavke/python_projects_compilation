48bd66135fb959febd43ae85a497b79d03d24702
Snavke <altheo.mananquil@gmail.com>
2025-03-13 01:53:34 +0800

    Functional Code, Final Commit

--- batch2_practice_program/even_number_counter.py
+++ batch2_practice_program/even_number_counter.py
@@ -1,10 +1,12 @@
 # initiliaze where to store the amount of even numbers are in the input
+count_of_even = 0
 
 # loop for entering 10 numbers
-
+for i in range (0,10):
+    numbers = float(input("Enter Number: "))
 # condition to determine if number is even
-  
+    if numbers % 2 == 0:
 # if condition is met, count as +1
-
+        count_of_even += 1
 # print the amount of even numbers
-    
+print (count_of_even)
