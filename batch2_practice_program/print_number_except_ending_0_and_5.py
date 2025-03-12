7b39b8084b86bab3f62725216955b8f6e21d078f
Snavke <altheo.mananquil@gmail.com>
2025-03-13 02:23:48 +0800

    Pseudo Code Changes, Functional Code, Final Commit

--- batch2_practice_program/print_number_except_ending_0_and_5.py
+++ batch2_practice_program/print_number_except_ending_0_and_5.py
@@ -1,5 +1,6 @@
-# use for loop and operator "and" to filter out anything divisible by 5
-
+# use for loop to create range from 1-100
+for i in range (0,100):
+# filter out numbers ending by 0 and 5 by using modulus by 5, if returned 0, then it is a number ending either in 5 or 0
+    if i % 5 != 0:
 # print number except ending 0 and 5
-
-
+        print (i)
