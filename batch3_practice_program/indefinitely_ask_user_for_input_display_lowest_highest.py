4b617c8a858c6812427bf5bdeb06cafbeb090629
Snavke <altheo.mananquil@gmail.com>
2025-03-14 02:38:47 +0800

    Displays Lists, Not Sorting Properly, 2nd Commit

--- batch3_practice_program/indefinitely_ask_user_for_input_display_lowest_highest.py
+++ batch3_practice_program/indefinitely_ask_user_for_input_display_lowest_highest.py
@@ -1,13 +1,16 @@
-# list to store inouts
-
+# list to store inputs
+number_inputs = []
 # indefinite loop asking for number
-
+while True:
+    numbers = (input("Enter Number:"))
 # check if input is not a number
-
+    if not numbers.isdigit():
 # terminates if non numeric value
-
+        break
 # convert string inputs to int
-
+    converted_int_number = int(numbers)
+    number_inputs.append(converted_int_number)
 # arrange list from lowest to highest using .sort function
-
+    number_inputs.sort
 # print list arranged from lowest to highest
+    print (number_inputs)
