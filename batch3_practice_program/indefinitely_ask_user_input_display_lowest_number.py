42e6f0f17489a22c3a0a61c0cd2b409c7e0d0f19
Snavke <altheo.mananquil@gmail.com>
2025-03-14 02:12:26 +0800

    Added way to safely compare inputs, Pseudo Code Changes, Functional Code, Final Commit

--- batch3_practice_program/indefinitely_ask_user_input_display_lowest_number.py
+++ batch3_practice_program/indefinitely_ask_user_input_display_lowest_number.py
@@ -7,8 +7,10 @@ while True:
     if not numbers.isdigit():
 # exit loop if non numeric value
         break
+     # safely convert string inputs to integer for safe comparisons
+    converted_int_number = int(numbers)
 # check for inputs for lowest
     if numbers not in smallest_number:
-        smallest_number.append(numbers)
+        smallest_number.append(converted_int_number)
 # print smallest number
     print (min(smallest_number))
