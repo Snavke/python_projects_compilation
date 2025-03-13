bc74a0c40f8deba980bd6489f63a9aa90c480141
Snavke <altheo.mananquil@gmail.com>
2025-03-14 02:10:53 +0800

    Functional, 2nd commit

--- batch3_practice_program/indefinitely_ask_user_input_display_lowest_number.py
+++ batch3_practice_program/indefinitely_ask_user_input_display_lowest_number.py
@@ -1,9 +1,14 @@
 # list to store inputs
-
+smallest_number = []
 # indefinite loop asking for number
-
+while True:
+    numbers = (input("Enter Number: "))
 # check if input is not a number
-
+    if not numbers.isdigit():
 # exit loop if non numeric value
-
+        break
 # check for inputs for lowest
+    if numbers not in smallest_number:
+        smallest_number.append(numbers)
+# print smallest number
+    print (min(smallest_number))
