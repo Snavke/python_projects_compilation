edfe7020f654ca5ce6a138863d40e55c6deda2f9
Snavke <altheo.mananquil@gmail.com>
2025-03-14 00:02:07 +0800

    Non Functional, Scrapping, 2nd Commit

--- batch3_practice_program/display_number_duplicate_only_first_entry.py
+++ batch3_practice_program/display_number_duplicate_only_first_entry.py
@@ -1,11 +1,15 @@
 # list to store inputs
-
+number_inputs = []
 # prompt user to enter 10 inputs using for loop
-
+for i in range (0,10):
+    numbers = int(input("Enter Number: "))
 # use .append to store inputs to existing list
-
+    number_inputs.append(numbers)
 # list to store numbers with duplicate
-
+duplicate_inputs = []
 # eliminate duplicated inputs 
-
+for item in number_inputs:
+    if number_inputs.count(item) == 2:
+        duplicate_inputs.append(item) 
 # print numbers 
+print (duplicate_inputs)
