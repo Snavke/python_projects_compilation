3aba9e06d937139e2d3d69872d840ab4cd69e77d
Snavke <altheo.mananquil@gmail.com>
2025-03-13 11:07:43 +0800

    Tested .append method, Works, 4th Commit

--- batch3_practice_program/display_numbers_no_duplicate.py
+++ batch3_practice_program/display_numbers_no_duplicate.py
@@ -1,7 +1,12 @@
 # use lists to store inputs
-
+number_inputs = []
 # prompt user to enter 10 inputs using for loop 
+for i in range (0,10):
+    numbers = int(input("Enter Number: "))
+    # use .append to add user inputs to existing list
+    number_inputs.append(numbers)
 
+print (number_inputs)
 # make way to determine if number is duplicated
 
 # print numbers without duplicate
