c2188a4307255d03ebf3c726a4eaff51b1f029a9
Snavke <altheo.mananquil@gmail.com>
2025-03-14 02:22:38 +0800

    Pseudo Code Draft, 1st Commit

---
+++ batch3_practice_program/indefinitely_ask_user_for_input_display_lowest_highest.py
@@ -0,0 +1,13 @@
+# list to store inouts
+
+# indefinite loop asking for number
+
+# check if input is not a number
+
+# terminates if non numeric value
+
+# convert string inputs to int
+
+# arrange list from lowest to highest using .sort function
+
+# print list arranged from lowest to highest
