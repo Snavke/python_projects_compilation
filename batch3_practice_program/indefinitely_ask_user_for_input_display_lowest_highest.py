1a5d6606379f9b926a2cbc03a1e56ce5eca8cd73
Snavke <altheo.mananquil@gmail.com>
2025-03-14 03:05:57 +0800

    Added Missing Parenthesis on sort function, Functional Code, Final Commit

--- batch3_practice_program/indefinitely_ask_user_for_input_display_lowest_highest.py
+++ batch3_practice_program/indefinitely_ask_user_for_input_display_lowest_highest.py
@@ -2,15 +2,16 @@
 number_inputs = []
 # indefinite loop asking for number
 while True:
-    numbers = (input("Enter Number:"))
+    numbers = (input("Enter Number: "))
 # check if input is not a number
     if not numbers.isdigit():
 # terminates if non numeric value
         break
 # convert string inputs to int
     converted_int_number = int(numbers)
+# add to lists
     number_inputs.append(converted_int_number)
 # arrange list from lowest to highest using .sort function
-    number_inputs.sort
+    number_inputs.sort()
 # print list arranged from lowest to highest
     print (number_inputs)
