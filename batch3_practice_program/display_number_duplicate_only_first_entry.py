64a3ea0145dfd06c6e76dd68ccbdbce6b01e3c5b
Snavke <altheo.mananquil@gmail.com>
2025-03-13 23:46:05 +0800

    Pseudo Code Draft, First Commit

---
+++ batch3_practice_program/display_number_duplicate_only_first_entry.py
@@ -0,0 +1,11 @@
+# list to store inputs
+
+# prompt user to enter 10 inputs using for loop
+
+# use .append to store inputs to existing list
+
+# list to store numbers with duplicate
+
+# eliminate duplicated inputs 
+
+# print numbers 
