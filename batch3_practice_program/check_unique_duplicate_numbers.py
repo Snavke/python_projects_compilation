f4af86eb20407a97e849bbcdb5dd940b7265f081
Snavke <altheo.mananquil@gmail.com>
2025-03-14 01:18:59 +0800

    Working, but terminates when duplicate number entered, 3rd Commit

--- batch3_practice_program/check_unique_duplicate_numbers.py
+++ batch3_practice_program/check_unique_duplicate_numbers.py
@@ -1,5 +1,5 @@
 # lists to store numbers entered by user
-
+number_inputs = []
 # a loop that indefinitely asks user for number
 while True:
     numbers = int(input("Enter Number: "))
@@ -7,7 +7,12 @@ while True:
 # exit loop
 
 # check input
-if numbers 
+    if numbers in number_inputs:
 # display message if entered number "unique" or "duplicated" after user enters number
+        print ("Duplicate")
+        break
+    else:
+        number_inputs.append(numbers)
+        print ("Unique")
 
 # terminate if non numerical value
