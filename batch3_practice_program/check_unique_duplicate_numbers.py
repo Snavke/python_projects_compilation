b37f0ebdae7e199e06ae60e54f38b5529e55be65
Snavke <altheo.mananquil@gmail.com>
2025-03-14 00:39:07 +0800

    Pseudo Code Draft, 1st Commit

---
+++ batch3_practice_program/check_unique_duplicate_numbers.py
@@ -0,0 +1,7 @@
+# a loop that indefinitely asks user for number
+
+# check input
+
+# display message if entered number "unique" or "duplicated" after user enters number
+
+# terminate if non numerical value
