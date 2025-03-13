ac1a30e86ec93b108547b8ad84a542619e7b5f36
Snavke <altheo.mananquil@gmail.com>
2025-03-14 01:01:17 +0800

    Pseudo Code Update, 2nd Commit

--- batch3_practice_program/check_unique_duplicate_numbers.py
+++ batch3_practice_program/check_unique_duplicate_numbers.py
@@ -1,7 +1,13 @@
+# lists to store numbers entered by user
+
 # a loop that indefinitely asks user for number
+while True:
+    numbers = int(input("Enter Number: "))
+
+# exit loop
 
 # check input
-
+if numbers 
 # display message if entered number "unique" or "duplicated" after user enters number
 
 # terminate if non numerical value
