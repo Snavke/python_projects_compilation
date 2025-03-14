0198b85d7488e63bed4dfd3e03ed9ac76dd8f1e8
Snavke <altheo.mananquil@gmail.com>
2025-03-14 14:44:08 +0800

    Variable Name Update

--- batch_4_practice_program/indefinitely_ask_user_input_print_number_most_duplicate.py
+++ batch_4_practice_program/indefinitely_ask_user_input_print_number_most_duplicate.py
@@ -1,24 +1,24 @@
 # dictionary to store inputs
-number_inputs = {}
+list_number_inputs = {}
 # indefinitely ask for user input
 while True:
-    numbers = input("Enter Number: ")
+    numbers_input = input("Enter Number: ")
 # check if input is not a number
-    if not numbers.isdigit():
+    if not numbers_input.isdigit():
 # terminate if non numerical value
         break
 # convert string inputs to int
-    converted_int_number = int(numbers)
+    converted_int_number = int(numbers_input)
 
 # count how many times a number shows up
-    if converted_int_number in number_inputs:
-        number_inputs[converted_int_number] += 1
+    if converted_int_number in list_number_inputs:
+        list_number_inputs[converted_int_number] += 1
     else:
-        number_inputs[converted_int_number] = 1
+        list_number_inputs[converted_int_number] = 1
 
     
 # scan for duplicates
-    if number_inputs:
-        most_frequent = max (number_inputs, key=number_inputs.get)
+    if list_number_inputs:
+        most_frequent = max (list_number_inputs, key=list_number_inputs.get)
 # print most duplicated 
         print (most_frequent)
