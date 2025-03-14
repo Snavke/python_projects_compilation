9176a4588598b9e74b31504266e4345d823254ed
Snavke <altheo.mananquil@gmail.com>
2025-03-14 14:34:49 +0800

    Variable Name Updated

--- batch_3_practice_program/display_number_duplicate_only_first_entry.py
+++ batch_3_practice_program/display_number_duplicate_only_first_entry.py
@@ -1,13 +1,13 @@
 # list to store inputs
-number_inputs = []
+list_number_inputs = []
 # prompt user to enter 10 inputs using for loop
 for i in range (0, 10):
-    numbers = int(input("Enter Number: "))
-    number_inputs.append(numbers)
+    numbers_input = int(input("Enter Number: "))
+    list_number_inputs.append(numbers_input)
 # store unique inputs
 unique_inputs = []
 # eliminate duplicated input using "not in"
-for item in number_inputs:
+for item in list_number_inputs:
 # check if already in list, if not add 
     if item not in unique_inputs:
 # if not add to unique list
