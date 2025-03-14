3eb2883ebeb5b2a152995aee72a65fe50edac930
Snavke <altheo.mananquil@gmail.com>
2025-03-14 11:30:47 +0800

    Pseudo Code Updated, Functional Code, Final Commit

--- batch_4_practice_program/print_number_with_duplicate.py
+++ batch_4_practice_program/print_number_with_duplicate.py
@@ -1,9 +1,17 @@
 # list to store inputs
-
+number_inputs = []
 # loop for entering 10 numbers
-
+for i in range (0, 10):
 # store variable to list
-
+    numbers = int(input("Enter Number: "))
+# add numbers to existing list
+    number_inputs.append(numbers)
+# list to store unique inputs
+duplicated_inputs = []
 # check for duplicate
+for item in number_inputs:
+    if number_inputs.count(item) == 2:
+        duplicated_inputs.append(item)
 
 # print duplicated numbers
+print (duplicated_inputs)
