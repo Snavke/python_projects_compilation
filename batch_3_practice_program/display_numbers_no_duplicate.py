0006533f0594f45fba0bc2bd9467b5cb5cf40fc5
Snavke <altheo.mananquil@gmail.com>
2025-03-14 11:31:13 +0800

    Format Changes, Final Commit

--- batch_3_practice_program/display_numbers_no_duplicate.py
+++ batch_3_practice_program/display_numbers_no_duplicate.py
@@ -1,7 +1,7 @@
 # use lists to store inputs
 number_inputs = []
 # prompt user to enter 10 inputs using for loop 
-for i in range (0,10):
+for i in range (0, 10):
     numbers = int(input("Enter Number: "))
     # use .append to add user inputs to existing list
     number_inputs.append(numbers)
