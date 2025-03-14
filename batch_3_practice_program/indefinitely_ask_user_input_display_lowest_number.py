a33d1f6ef45290e64c4c1f37421a0bbfd70b882f
Snavke <altheo.mananquil@gmail.com>
2025-03-14 14:42:06 +0800

    Variable Name Update

--- batch_3_practice_program/indefinitely_ask_user_input_display_lowest_number.py
+++ batch_3_practice_program/indefinitely_ask_user_input_display_lowest_number.py
@@ -1,16 +1,16 @@
 # list to store inputs
-smallest_number = []
+list_smallest_number = []
 # indefinite loop asking for number
 while True:
-    numbers = (input("Enter Number: "))
+    numbers_input = (input("Enter Number: "))
 # check if input is not a number
-    if not numbers.isdigit():
+    if not numbers_input.isdigit():
 # exit loop if non numeric value
         break
      # safely convert string inputs to integer for safe comparisons
-    converted_int_number = int(numbers)
+    converted_int_number = int(numbers_input)
 # check for inputs for lowest
-    if numbers not in smallest_number:
-        smallest_number.append(converted_int_number)
+    if numbers_input not in list_smallest_number:
+        list_smallest_number.append(converted_int_number)
 # print smallest number
-    print (min(smallest_number))
+    print (min(list_smallest_number))
