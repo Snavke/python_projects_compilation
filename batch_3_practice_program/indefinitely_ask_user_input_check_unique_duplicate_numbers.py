a04d6c1ae8fec1a95e6a9e45f9980c0a26fe8eaa
Snavke <altheo.mananquil@gmail.com>
2025-03-14 14:40:44 +0800

    Variable Name Update

--- batch_3_practice_program/indefinitely_ask_user_input_check_unique_duplicate_numbers.py
+++ batch_3_practice_program/indefinitely_ask_user_input_check_unique_duplicate_numbers.py
@@ -1,16 +1,16 @@
 # lists to store numbers entered by user
-number_inputs = []
+list_number_input = []
 # a loop that indefinitely asks user for number
 while True:
-    numbers = (input("Enter Number: "))
+    numbers_input = (input("Enter Number: "))
 # check if input is not a number
-    if not numbers.isdigit():
+    if not numbers_input.isdigit():
         # terminate if non numerical value
         break
 # check input
-    if numbers in number_inputs:
+    if numbers_input in list_number_input:
 # display message if entered number "unique" or "duplicated" after user enters number
         print ("Duplicate")
     else:
-        number_inputs.append(numbers)
+        list_number_input.append(numbers_input)
         print ("Unique")
