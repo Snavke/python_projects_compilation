8777f0ec4a63fcede3eb1f095122999f12d34821
Snavke <altheo.mananquil@gmail.com>
2025-03-14 13:07:50 +0800

    Pseudo Code Updated, Functional Code, Final Commit

--- batch_4_practice_program/print_number_most_duplicate.py
+++ batch_4_practice_program/print_number_most_duplicate.py
@@ -1,17 +1,24 @@
-# list to store inputs
-number_inputs = []
+# dictionary to store inputs
+number_inputs = {}
 # indefinitely ask for user input
 while True:
-    numbers = (input("Enter Number: "))
+    numbers = input("Enter Number: ")
 # check if input is not a number
     if not numbers.isdigit():
 # terminate if non numerical value
         break
 # convert string inputs to int
     converted_int_number = int(numbers)
-# add to list
-    number_inputs.append(converted_int_number)
-# list to add duplicated numbers
-duplicated_inputs = []
-# scan for duplicates
 
+# count how many times a number shows up
+    if converted_int_number in number_inputs:
+        number_inputs[converted_int_number] += 1
+    else:
+        number_inputs[converted_int_number] = 1
+
+    
+# scan for duplicates
+    if number_inputs:
+        most_frequent = max (number_inputs, key=number_inputs.get)
+# print most duplicated 
+        print (most_frequent)
