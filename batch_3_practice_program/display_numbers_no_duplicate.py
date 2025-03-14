90d16b72e0fe46dfeaf3d91cfb3d043e9c387771
Snavke <altheo.mananquil@gmail.com>
2025-03-14 14:37:31 +0800

    Variable Name Updated

--- batch_3_practice_program/display_numbers_no_duplicate.py
+++ batch_3_practice_program/display_numbers_no_duplicate.py
@@ -1,18 +1,18 @@
 # use lists to store inputs
-number_inputs = []
+list_number_input = []
 # prompt user to enter 10 inputs using for loop 
 for i in range (0, 10):
-    numbers = int(input("Enter Number: "))
+    numbers_input = int(input("Enter Number: "))
     # use .append to add user inputs to existing list
-    number_inputs.append(numbers)
+    list_number_input.append(numbers_input)
 
 # create list to store unique numbers
 unique_inputs = []
 
 # determine if a number is duplicated
-for item in number_inputs:
+for item in list_number_input:
     # if values in "number_inputs" is equal to one, we add it to the list "unique_inputs"
-    if number_inputs.count(item) == 1:
+    if list_number_input.count(item) == 1:
         unique_inputs.append(item)
 
 # print numbers without duplicate
