ea13ff2d9279bde532c6c39957b36246a5f03a4c
Snavke <altheo.mananquil@gmail.com>
2025-03-14 14:38:51 +0800

    Variable Name Update

--- batch_3_practice_program/indefinitely_ask_user_for_input_display_lowest_highest.py
+++ batch_3_practice_program/indefinitely_ask_user_for_input_display_lowest_highest.py
@@ -1,17 +1,17 @@
 # list to store inputs
-number_inputs = []
+list_number_input = []
 # indefinite loop asking for number
 while True:
-    numbers = (input("Enter Number: "))
+    numbers_input = (input("Enter Number: "))
 # check if input is not a number
-    if not numbers.isdigit():
+    if not numbers_input.isdigit():
 # terminates if non numeric value
         break
 # convert string inputs to int
-    converted_int_number = int(numbers)
+    converted_int_number = int(numbers_input)
 # add to lists
-    number_inputs.append(converted_int_number)
+    list_number_input.append(converted_int_number)
 # arrange list from lowest to highest using .sort function
-    number_inputs.sort()
+    list_number_input.sort()
 # print list arranged from lowest to highest
-    print (number_inputs)
+    print (list_number_input)
