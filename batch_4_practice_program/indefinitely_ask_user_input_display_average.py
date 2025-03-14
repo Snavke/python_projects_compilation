251f355b5c7e77b693dd6896c832f48d797e3b7b
Snavke <altheo.mananquil@gmail.com>
2025-03-14 14:00:18 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_4_practice_program/indefinitely_ask_user_input_display_average.py
@@ -0,0 +1,19 @@
+# variable to store sum of numbers
+
+# variable to store count of numbers entered
+
+# indefinite loop asking for number
+
+# check if input is not a number
+
+# terminates if non numeric value
+
+# convert string inputs to int
+
+# add numbers to the sum
+
+# increase count
+
+# calculate average
+
+# print average 
