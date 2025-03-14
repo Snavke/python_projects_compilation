beaecf6d32cba909737b6df91efc2455a71befb2
Snavke <altheo.mananquil@gmail.com>
2025-03-14 13:26:03 +0800

    Pseudo Code Draft, 1st Commit

---
+++ batch_4_practice_program/indefinitely_ask_user_input_print_highest_number.py
@@ -0,0 +1,13 @@
+# variable to store highest number
+
+# indefinite loop asking for number
+
+# check if input is not a number
+
+# exit loop if non numeric value
+
+# convert input to integer
+
+# update highest number
+
+# display highest number
