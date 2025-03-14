b71718a02b8300b3ffb22b59c82272a527b84270
Snavke <altheo.mananquil@gmail.com>
2025-03-14 11:54:46 +0800

    Pseudo Code Draft, 1st Commit

---
+++ batch_4_practice_program/print_number_most_duplicate.py
@@ -0,0 +1,17 @@
+# list to store inputs
+
+# indefinitely ask for user input
+
+# check if input is not a number
+
+# terminate if non numerical value
+
+# convert string inputs to int
+
+# add to list
+
+# scan for duplicates
+
+# eliminate other number if already in list
+
+# print number with most duplicate
