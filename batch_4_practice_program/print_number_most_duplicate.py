355440a745fd8a1cb4d8b11f9a1947aec50359e0
Snavke <altheo.mananquil@gmail.com>
2025-03-14 12:29:36 +0800

    Returning empty lists, 2nd Commit

--- batch_4_practice_program/print_number_most_duplicate.py
+++ batch_4_practice_program/print_number_most_duplicate.py
@@ -1,17 +1,20 @@
 # list to store inputs
-
+number_inputs = []
+duplicated_inputs = []
 # indefinitely ask for user input
-
+while True:
+    numbers = (input("Enter Number: "))
 # check if input is not a number
-
+    if not numbers.isdigit():
 # terminate if non numerical value
-
+        break
 # convert string inputs to int
-
+    converted_int_number = int(numbers)
 # add to list
-
+    number_inputs.append(converted_int_number)
 # scan for duplicates
-
-# eliminate other number if already in list
-
+    if number_inputs.count(converted_int_number) == 2:
+        # eliminate other number if already in list
+        duplicated_inputs.append(converted_int_number)          
 # print number with most duplicate
+    print (duplicated_inputs)
