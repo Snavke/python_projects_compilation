dc5c51d13cf0c59762018672ccfa019f001a975b
Snavke <altheo.mananquil@gmail.com>
2025-03-20 02:16:00 +0800

    Moved File to Appropriate Locatiom

---
+++ batch_5_practice_program/eliminate_spaces_in_beginning_full_name.py
@@ -0,0 +1,5 @@
+# Prompt user to enter name with space & use.strip string method to eliminate spaces 
+full_name = str(input(" Enter Full Name wtih spaces in the beginning: ")).strip()
+
+# Print full name without spaces
+print (full_name)
