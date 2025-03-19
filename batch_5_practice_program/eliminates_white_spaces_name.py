e39c8b787ea2f9bf3bb0ae63f78950b883aa6354
Snavke <altheo.mananquil@gmail.com>
2025-03-20 02:05:11 +0800

    Functional Code, Renamed File, Final Commit

--- batch_5_practice_program/eliminates_white_spaces_name.py
+++ batch_5_practice_program/eliminates_white_spaces_name.py
@@ -1,3 +1,4 @@
 # Prompt user to enter name with multiple spaces in the beginning
-
+full_name = str(input(" Enter Full Name wtih spaces in the beginning: ")).strip()
 # print without white spaces using .strip string method
+print (full_name)
