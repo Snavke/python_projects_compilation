3de5a1070a3fbc1f8ee17cebe30bfbb80c77a121
Snavke <altheo.mananquil@gmail.com>
2025-03-20 15:42:19 +0800

    Minor Changes

---
+++ batch_5_practice_program/count_of_characters_full_name.py
@@ -0,0 +1,8 @@
+# Prompt user to enter full Name
+full_name = str(input("Please Enter Full Name: "))
+
+# Count the amount of characters & store count to a variable
+count_of_character = len(full_name) 
+
+# Print count
+print (count_of_character)
