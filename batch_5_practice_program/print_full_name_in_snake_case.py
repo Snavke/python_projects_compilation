db17c1315fc46ad9caa28fae43f931683279d496
Snavke <altheo.mananquil@gmail.com>
2025-03-20 16:10:10 +0800

    Functional Code, Final Commit

--- batch_5_practice_program/print_full_name_in_snake_case.py
+++ batch_5_practice_program/print_full_name_in_snake_case.py
@@ -1,2 +1,4 @@
 # Prompt user to enter full name & Convert string input to .lower, change spaces to _ with .replace
+full_name = str(input("Please Enter Full Name: ")).lower().replace(" ", "_")
 # Print full name in Snake Case
+print (full_name)
