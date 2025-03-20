af517c7379b74e4f0d776fdc222e0eb934fd1b62
Snavke <altheo.mananquil@gmail.com>
2025-03-20 14:58:38 +0800

    Functional Code, Final Commit

--- batch_5_practice_program/count_of_words.py
+++ batch_5_practice_program/count_of_words.py
@@ -1,7 +1,8 @@
 # Prompt user to enter a sentence
+sentence = str(input("Please Enter a Sentence: "))
 
-# Count the amount of words 
-
-# Store count of words
+# Count the amount of words & store count to a variable
+count_of_word = len(sentence) 
 
 # Print count
+print (count_of_word)
