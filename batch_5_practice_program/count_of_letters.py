bc1c78ea83cff1fac849df466212fc61ac21800b
Snavke <altheo.mananquil@gmail.com>
2025-03-20 15:45:15 +0800

    Deleted

--- batch_5_practice_program/count_of_letters.py
+++
@@ -1,7 +0,0 @@
-# Prompt user to enter full name
-
-# Count letters
-
-# Store letter count
-
-# Print count of letters
--- batch_5_practice_program/count_of_words.py
+++
@@ -1,7 +0,0 @@
-# Prompt user to enter a sentence
-
-# Count words
-
-# Store word count
-
-# Print count of word
