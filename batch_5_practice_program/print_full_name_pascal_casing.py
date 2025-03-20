a4cb897db4cd02ca6e8599ffaf1d7844c5892c00
Snavke <altheo.mananquil@gmail.com>
2025-03-20 16:00:41 +0800

    Functional Code, Final Commit

--- batch_5_practice_program/print_full_name_pascal_casing.py
+++ batch_5_practice_program/print_full_name_pascal_casing.py
@@ -1,5 +1,5 @@
-# Prompt user to enter full name
-
-# Convert string input to title casing, eliminate spaces with .replace
+# Prompt user to enter full name & Convert string input to .title, eliminate spaces with .replace
+full_name = str(input("Please Enter Full Name: ")).title().replace(" ", "")
 
 # Print full name in Pascal Casing
+print (full_name)
