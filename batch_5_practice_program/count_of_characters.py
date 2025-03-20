f3e7835864c061cdd8050424ee8e76ef50fcc4a6
Snavke <altheo.mananquil@gmail.com>
2025-03-20 15:27:55 +0800

    Rename File, Corrected Variable Name

---
+++ batch_5_practice_program/count_of_characters.py
@@ -0,0 +1,8 @@
+# Prompt user to enter a sentence
+sentence = str(input("Please Enter a Sentence: "))
+
+# Count the amount of characters & store count to a variable
+count_of_character = len(sentence) 
+
+# Print count
+print (count_of_character)
