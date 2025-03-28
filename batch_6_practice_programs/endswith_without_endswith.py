7f7fdf47407ca9ecf121b08a8f0644f389f51539
Snavke <altheo.mananquil@gmail.com>
2025-03-28 21:21:43 +0800

    Functional Code, Final Commit

--- batch_6_practice_programs/endswith_without_endswith.py
+++ batch_6_practice_programs/endswith_without_endswith.py
@@ -1,7 +1,13 @@
 # prompt to get main string
+user_input = str(input("Enter a String of Characters: "))
 
 # prompt to get suffix
+user_input_suffix = str(input("Enter Suffix to Remove: "))
 
 # compare last characters of text with suffix
+if user_input[-len(user_input_suffix):] == user_input_suffix:
+    print ("String ENDS with the given suffix.")
 
 # print to tell if string ends or does not end with given suffix
+else: 
+    print ("String does NOT END with the given suffix.")
