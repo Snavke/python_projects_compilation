d648ec932bfefaee659e89d02e2fe7f64a1c5b9d
Snavke <altheo.mananquil@gmail.com>
2025-03-28 21:07:14 +0800

    Functional Code, Final Commit

--- batch_6_practice_programs/isupper_without_isupper.py
+++ batch_6_practice_programs/isupper_without_isupper.py
@@ -1,9 +1,15 @@
 # prompt user to get main string
+user_input = str(input("Enter a String: "))
 
 # boolean that assumes string is already in uppercase
+uppercase = True
 
 # for loop to check for lowercase characters
-
+for char in user_input:
+    if 'a' <= char <= 'z':
+        uppercase = False
 # exit loop if lowercase is found
+        break
 
-# print true or false according to user input
+# returns true or false value for uppercase according to user input
+print ("Characters in Uppercase?", uppercase)
