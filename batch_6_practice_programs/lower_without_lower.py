a79c2b8c0a17f88072b5aea720455c57c5df0a98
Snavke <altheo.mananquil@gmail.com>
2025-03-28 20:54:45 +0800

    Functional Code, Final Commit

--- batch_6_practice_programs/lower_without_lower.py
+++ batch_6_practice_programs/lower_without_lower.py
@@ -1,9 +1,20 @@
 # prompt user to get main string
+user_input = str(input("Enter a String of Characters: "))
 
 # initialize a variable to store the lowercase string
+converted_lowercase = ""
 
+# reference variable
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase = "abcdefghijklmnopqrstuvwxyz"
 # use for loop to check if character is in uppercase
-
+for char in user_input: 
+    if char in uppercase:
+        index = uppercase.index(char)
+        converted_lowercase += lowercase[index]
 # keep lowercase characters unchanged
-
+    else: 
+        converted_lowercase += char
 # print string converted to lowercase
+print (converted_lowercase)
+ 
