9306f2887afe6d065696b32d38453ec97fab4d56
Snavke <altheo.mananquil@gmail.com>
2025-03-28 21:56:24 +0800

    Functional Code, Final Commit

--- batch_6_practice_programs/swapcase_without_swapcase.py
+++ batch_6_practice_programs/swapcase_without_swapcase.py
@@ -1,7 +1,20 @@
 # prompt user to get main string
+user_input = str(input("Enter String: "))
 
 # variable to store swapcased string
+swapcase = ""
 
 # for loop to swapcases
+for char in user_input:
+# if lowercase then convert to uppercase
+    if char in "abcdefghijklmnopqrstuvwxyz":
+        swapcase += char.upper()
+# if uppercase then convert to lowercase
+    elif char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        swapcase += char.lower()
+# ignore non alphabet characters
+    else: 
+        swapcase += char
 
 # print swapcased version 
+print (swapcase)
