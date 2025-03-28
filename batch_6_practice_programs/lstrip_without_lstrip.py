4c84934f8d3bd1e0f3a3f6de4a1a085a0dc4864f
Snavke <altheo.mananquil@gmail.com>
2025-03-28 20:06:15 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_6_practice_programs/lstrip_without_lstrip.py
@@ -0,0 +1,7 @@
+# prompt user to enter a character with spaces before the characters
+
+# use index method to track place in string
+
+# while loop to find the non-space character
+
+# remove leading spaces
