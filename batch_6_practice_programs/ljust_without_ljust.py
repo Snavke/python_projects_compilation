0ce958c4a2cfadbff6b522c7c729173071035989
Snavke <altheo.mananquil@gmail.com>
2025-03-28 21:32:07 +0800

    Functional Code, Final Commit

--- batch_6_practice_programs/ljust_without_ljust.py
+++ batch_6_practice_programs/ljust_without_ljust.py
@@ -1,7 +1,12 @@
 # prompt user to get main string
+user_input = str(input("Enter String: "))
 
 # prompt to ask user the desired width
+desired_width = int(input("Enter Desired Total Width: "))
 
 # if the string is shorter than the width, add spaces to right
+if len(user_input) < desired_width:
+    user_input = user_input + " " * (desired_width - len(user_input))
 
-# print adjusted string
+# print adjusted string with visual markers |  | to show visually tell if spaces are added 
+print (f"|{user_input}|")
