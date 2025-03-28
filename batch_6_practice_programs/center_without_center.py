b433136aeddb20992de7179eb29686f105e93ad1
Snavke <altheo.mananquil@gmail.com>
2025-03-28 21:37:31 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_6_practice_programs/center_without_center.py
@@ -0,0 +1,7 @@
+# prompt user to get main string
+
+# prompt to ask user desired width
+
+# determine the number of spaces needed through calculations
+
+# print adjusted string with visual markers 
