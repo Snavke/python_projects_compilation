9bc72d9381e494af80cfad10e0d934c295f6f44b
Snavke <altheo.mananquil@gmail.com>
2025-03-28 20:30:14 +0800

    Functional Code, Final Commit

--- batch_6_practice_programs/removeprefix_without_removeprefix.py
+++ batch_6_practice_programs/removeprefix_without_removeprefix.py
@@ -1,9 +1,14 @@
 # prompt to get main string
+user_input = str(input("Enter a String of Characters: "))
 
 # prompt to get prefix
+user_input_prefix = str(input("Enter Prefix to Remove: "))
 
 # check if input starts with prefix
+if user_input.startswith(user_input_prefix):
 
 # remove prefix
-
+    user_input = user_input[len(user_input_prefix):]
+    
 # print string without prefix
+print (user_input)
