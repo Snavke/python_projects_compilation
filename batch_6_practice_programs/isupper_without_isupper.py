c4a7396631560b6d0a1ce602aa0744d6e2844f8d
Snavke <altheo.mananquil@gmail.com>
2025-03-28 21:03:37 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_6_practice_programs/isupper_without_isupper.py
@@ -0,0 +1,9 @@
+# prompt user to get main string
+
+# boolean that assumes string is already in uppercase
+
+# for loop to check for lowercase characters
+
+# exit loop if lowercase is found
+
+# print true or false according to user input
