9cf8a789716da18f1f417943e2e73dfbec3cb8bf
Snavke <altheo.mananquil@gmail.com>
2025-03-28 21:42:14 +0800

    Functional Code, Final Commit

--- batch_6_practice_programs/center_without_center.py
+++ batch_6_practice_programs/center_without_center.py
@@ -1,7 +1,15 @@
 # prompt user to get main string
+user_input = str(input("Enter String: "))
 
 # prompt to ask user desired width
+desired_width = int(input("Enter Total Width: "))
 
 # determine the number of spaces needed through calculations
+if len(user_input) < desired_width:
+    total_space = desired_width - len(user_input)
+    left_space = total_space // 2
+    right_space = total_space - left_space 
+    user_input = " " * left_space + user_input + " " * right_space
 
 # print adjusted string with visual markers 
+print (f"|{user_input}|")
