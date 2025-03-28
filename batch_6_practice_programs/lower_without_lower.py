b51d0a0ff470e1d459cb8b9b2f505ee8ac0ef7ab
Snavke <altheo.mananquil@gmail.com>
2025-03-28 20:36:27 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_6_practice_programs/lower_without_lower.py
@@ -0,0 +1,9 @@
+# prompt user to get main string
+
+# initialize a variable to store the lowercase string
+
+# use for loop to check if character is in uppercase
+
+# keep lowercase characters unchanged
+
+# print string converted to lowercase
