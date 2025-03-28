7241acca728e3d7946ad15b458381e19dcacc1dc
Snavke <altheo.mananquil@gmail.com>
2025-03-28 20:16:38 +0800

    Functional Code, Final Commit

--- batch_6_practice_programs/lstrip_without_lstrip.py
+++ batch_6_practice_programs/lstrip_without_lstrip.py
@@ -1,7 +1,18 @@
 # prompt user to enter a character with spaces before the characters
+user_input = str(input('Enter Charaters with Left Leading Spaces [Ex: "   Hello World"]: '))
 
 # use index method to track place in string
+index = 0
 
 # while loop to find the non-space character
+while index < len(user_input) and user_input[index] == " ":
+
+# increment index by 1
+    index += 1  
 
 # remove leading spaces
+user_input = user_input[index:]
+
+# print user input without left leading spaces
+print(user_input)
+
