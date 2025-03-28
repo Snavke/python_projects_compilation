d814e6711236e555b6ea5376c6c3d9504e8a0e49
Snavke <altheo.mananquil@gmail.com>
2025-03-28 22:09:33 +0800

    Functional Code, Final Commit

--- batch_6_practice_programs/capitalize_without_capitalize.py
+++ batch_6_practice_programs/capitalize_without_capitalize.py
@@ -1,9 +1,15 @@
 # get user input
+user_input = str(input("Enter String: "))
 
 # check if input is not empty
+if user_input:
 
 # convert first letter to uppercase, the rest in lowercase
+    capitalized = user_input[0].upper() + user_input[1:].lower()
 
 # ignore empty iput
+else:
+    capitalized = user_input
 
 # print capitalized input
+print (capitalized)
