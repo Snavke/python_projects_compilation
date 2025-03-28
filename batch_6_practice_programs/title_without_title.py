e8694749be8f79f514bbb89aae60cfeadb6b8c47
Snavke <altheo.mananquil@gmail.com>
2025-03-28 22:16:01 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_6_practice_programs/title_without_title.py
@@ -0,0 +1,11 @@
+# prompt user input to get main string
+
+# split input into words by using .split()
+
+# variable to store string converted to title
+
+# for loop to capitalize first letter of each word and leave the rest in lowercase
+
+# remove spaces at the end
+
+# print string converted to tile case
