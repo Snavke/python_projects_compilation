764dde0001a37aeb104a155ff0fe3cc63f4bbe02
Snavke <altheo.mananquil@gmail.com>
2025-04-01 05:29:06 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_7_practice_programs/rjust_without_rjust.py
@@ -0,0 +1,7 @@
+# prompt user to get main sring
+
+# prompt to ask user the desired width
+
+# if the string is shorter than the width, add spaces to right
+
+# print adjusted string with visual markers
