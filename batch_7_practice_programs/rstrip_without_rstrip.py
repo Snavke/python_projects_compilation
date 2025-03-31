73df1b09e3b61e9f274cf8a24b3495d73e82626e
Snavke <altheo.mananquil@gmail.com>
2025-04-01 04:44:13 +0800

    Functional Code, Has Some Bugs, Second Commit

--- batch_7_practice_programs/rstrip_without_rstrip.py
+++ batch_7_practice_programs/rstrip_without_rstrip.py
@@ -1,11 +1,17 @@
 # prompt user to enter a character with spaces after the characters
+user_input = str(input('Enter a String with Right Leading Spaces [Ex: "Hello World      "]: '))
 
 # use index method to track place in string but start at last character
+index = len(user_input) - 1
 
 # while loop to find non-space character
+while index >= 0 and user_input[index] == " ":
 
 # move left in the string by reduction of index by 1
+    index -= 1
 
 # remove trailing spaces
+user_input = user_input[:index + 1]
 
 # print user input without left leading spaces
+print (user_input)
