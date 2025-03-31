ed6a6e86e767e251cf57dc2ee6ac6c5e2b7dd69d
Snavke <altheo.mananquil@gmail.com>
2025-04-01 05:00:54 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_7_practice_programs/upper_without_upper.py
@@ -0,0 +1,11 @@
+# prompt user to get main string
+
+# initialize a variable to store the uppercase string
+
+# reference variable
+
+# use for loop to check if character is in lowercase
+
+# keep uppercase characters unchanged
+
+# print string converted to uppercase
