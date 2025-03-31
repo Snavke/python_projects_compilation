33ab8ab92bd89f75b1fd0a7c41402048414c74f5
Snavke <altheo.mananquil@gmail.com>
2025-04-01 04:57:31 +0800

    Functional Code, Final Commit

--- batch_7_practice_programs/removesuffix_without_removesuffix.py
+++ batch_7_practice_programs/removesuffix_without_removesuffix.py
@@ -1,9 +1,14 @@
 # prompt to get main string
+user_input = str(input("Enter a String of Characters: "))
 
 # prompt to get suffix
+user_input_suffix = str(input("Enter Suffix to Remove: "))
 
 # check if input ends with suffix
+if user_input.endswith(user_input_suffix):
 
 # remove suffix
+    user_input = user_input[:-len(user_input_suffix):]
 
 # print string without suffix
+print (user_input)
