37db26de44a304560ee256b856c831a9bee04ee4
Snavke <altheo.mananquil@gmail.com>
2025-04-01 05:14:38 +0800

    Functional Code, Final Commit

--- batch_7_practice_programs/islower_without_islower.py
+++ batch_7_practice_programs/islower_without_islower.py
@@ -1,9 +1,16 @@
 # prompt user to get main string
+user_input = str(input("Enter a String: "))
 
 # boolean that assumes string is already in lowercase
+lowercase = True
 
 # for loop to check for uppercase characters
+for char in user_input:
+    if 'A' <= char <= 'Z':
+        lowercase = False
 
 # exit loop if uppercase is found
+        break
 
 # returns true or false for lowercase according to user input
+print ("Characters in Lowercase?", lowercase)
