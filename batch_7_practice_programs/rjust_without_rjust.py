0fa8e513f11f0f81ecc5c9cbaec5ba94141c79cf
Snavke <altheo.mananquil@gmail.com>
2025-04-01 05:35:55 +0800

    Functional Code, Final Commit

--- batch_7_practice_programs/rjust_without_rjust.py
+++ batch_7_practice_programs/rjust_without_rjust.py
@@ -1,7 +1,12 @@
 # prompt user to get main sring
+user_input = str(input("Enter String: "))
 
 # prompt to ask user the desired width
+desired_width = int(input("Enter Desired Total Widt: "))
 
-# if the string is shorter than the width, add spaces to right
+# if the string is shorter than the width, add spaces to left
+if len(user_input) < desired_width:
+    user_input = " " * (desired_width - len(user_input)) + user_input
 
 # print adjusted string with visual markers
+print (f"|{user_input}|")
