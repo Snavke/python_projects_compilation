c2a2bd626910c92a8d948bf2caa6619365193f1d
Snavke <altheo.mananquil@gmail.com>
2025-04-01 06:05:11 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_7_practice_programs/index_without_index.py
@@ -0,0 +1,7 @@
+# prompt user to get main string
+
+# prompt to ask user to input substring to search for
+
+# for loop to loop through each character of string
+
+# check if substring matches current position
