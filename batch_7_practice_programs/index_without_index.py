950639c8160ccef52168ee8cc29d5fbb41cbcc41
Snavke <altheo.mananquil@gmail.com>
2025-04-01 06:13:32 +0800

    Functional Code, Final Commit

--- batch_7_practice_programs/index_without_index.py
+++ batch_7_practice_programs/index_without_index.py
@@ -1,7 +1,16 @@
 # prompt user to get main string
+user_input = str(input("Enter String: "))
 
 # prompt to ask user to input substring to search for
+substring_to_search = str(input("Enter the Substring to Search: "))
 
 # for loop to loop through each character of string
+for index_position in range(len(user_input)):
 
 # check if substring matches current position
+    if user_input [index_position:index_position+len(substring_to_search)] == substring_to_search:
+        print (f"Substring '{substring_to_search}' Located at Index {index_position}")
+        break
+# prints a message sin case the loops finishes without finding the substring to search
+else:
+    print(f"'{substring_to_search} nowhere to be found in the string.'")
