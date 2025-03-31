4e1599cc4c6759003d7ac8154cf9dbeff5f03a06
Snavke <altheo.mananquil@gmail.com>
2025-04-01 04:32:17 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_7_practice_programs/rstrip_without_rstrip.py
@@ -0,0 +1,11 @@
+# prompt user to enter a character with spaces after the characters
+
+# use index method to track place in string but start at last character
+
+# while loop to find non-space character
+
+# move left in the string by reduction of index by 1
+
+# remove trailing spaces
+
+# print user input without left leading spaces
