70fd6c53e7883fe2cab06a2cf87d986fab2b5e7d
Snavke <altheo.mananquil@gmail.com>
2025-04-01 05:26:33 +0800

    Functional Code, Final Commit

--- batch_7_practice_programs/startswith_without_startswith.py
+++ batch_7_practice_programs/startswith_without_startswith.py
@@ -1,7 +1,13 @@
 # prompt to get main string
+user_input = str(input("Enter a String of Characters: ")).lower()
 
 # prompt to get prefix
+user_input_prefix = str(input("Enter Prefix to Check: ")).lower()
 
 # compare first character of text with prefix
+if user_input[:len(user_input_prefix)] == user_input_prefix:
+    print ("String STARTS with the given prefix.")
 
 # print to tell if string starts or does not start with given prefix
+else:
+    print ("String does NOT START with the given prefix.")
