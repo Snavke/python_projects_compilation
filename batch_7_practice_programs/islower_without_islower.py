d9dce9b327b4074ab5b9a3401f6db24072b5fc31
Snavke <altheo.mananquil@gmail.com>
2025-04-01 05:08:51 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_7_practice_programs/islower_without_islower.py
@@ -0,0 +1,9 @@
+# prompt user to get main string
+
+# boolean that assumes string is already in lowercase
+
+# for loop to check for uppercase characters
+
+# exit loop if uppercase is found
+
+# returns true or false for lowercase according to user input
