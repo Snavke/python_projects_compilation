760f08e38662b11654bca65796e137287eeb0a3d
Snavke <altheo.mananquil@gmail.com>
2025-04-01 05:05:52 +0800

    Functional Code, Final Commit

--- batch_7_practice_programs/upper_without_upper.py
+++ batch_7_practice_programs/upper_without_upper.py
@@ -1,11 +1,22 @@
 # prompt user to get main string
+user_input = str(input("Enter a String of Characters: "))
 
 # initialize a variable to store the uppercase string
+converted_uppercase = ""
 
 # reference variable
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # use for loop to check if character is in lowercase
+for char in user_input:
+    if char in lowercase:
+        index = lowercase.index(char)
+        converted_uppercase += uppercase[index]
 
 # keep uppercase characters unchanged
+    else: 
+        converted_uppercase += char
 
 # print string converted to uppercase
+print (converted_uppercase)
