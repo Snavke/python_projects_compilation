bb7016ebd630ebbac16aa516e629edbe5f70291f
Snavke <altheo.mananquil@gmail.com>
2025-04-01 05:51:01 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_7_practice_programs/count_without_count.py
@@ -0,0 +1,10 @@
+# prompt user to get main string
+
+# prompt user to enter the substring they want to count
+
+# initialize a variable for counting the substring
+
+# for loop for string and substring
+
+# print the occurence of substring
+
