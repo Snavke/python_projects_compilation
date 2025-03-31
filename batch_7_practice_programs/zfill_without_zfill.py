ad51407865825e54fd6f752c71f9a2c2ee403617
Snavke <altheo.mananquil@gmail.com>
2025-04-01 05:43:42 +0800

    Functional Code, Final Commit

--- batch_7_practice_programs/zfill_without_zfill.py
+++ batch_7_practice_programs/zfill_without_zfill.py
@@ -1,8 +1,12 @@
 # prompt user to get main string
+user_input = str(input("Enter String: "))
 
 # prompt to ask user the desired width 
+desired_width = int(input("Enter Desired Total Width: "))
 
 # if the string is shorter than the width, add zeros to the left
+if len(user_input):
+    user_input = "0" * (desired_width - len(user_input)) + user_input
 
 # print string filled with zeros
-
+print (user_input)
