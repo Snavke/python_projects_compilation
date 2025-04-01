5ece186d5ac1bff8898c9de8a88512eb5040fa76
Snavke <altheo.mananquil@gmail.com>
2025-04-01 22:46:17 +0800

    Pseudo Code Draft, First Commit

---
+++ batch_7_practice_programs/rindex_without_rindex.py
@@ -0,0 +1,9 @@
+# prompt user to get main string
+
+# prompt to ask user to input substring to search for
+
+# loop through each character of string from the end
+
+# check if substring matches at current position
+
+# prionts a message in case the loop finishes without finding a substring
