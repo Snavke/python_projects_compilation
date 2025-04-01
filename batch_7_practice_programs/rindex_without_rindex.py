a7c34af14fcf07257a4c55a9cfcddc58bbb8d8fa
Snavke <altheo.mananquil@gmail.com>
2025-04-01 23:01:19 +0800

    Functional Code, Final Commit

--- batch_7_practice_programs/rindex_without_rindex.py
+++ batch_7_practice_programs/rindex_without_rindex.py
@@ -1,9 +1,17 @@
 # prompt user to get main string
+user_input = str(input("Enter String: "))
 
 # prompt to ask user to input substring to search for
+substring_to_search = str(input("Enter the Substring to Search: "))
 
 # loop through each character of string from the end
+for index_position in range(len(user_input) - len(substring_to_search), -1, -1):
 
-# check if substring matches at current position
+# check if substring matches at current 
+    if user_input[index_position:index_position + len(substring_to_search)] == substring_to_search:
+        print (f"Substring '{substring_to_search}' Located at Index {index_position}")
+        break
 
-# prionts a message in case the loop finishes without finding a substring
+# prints a message in case the loop finishes without finding a substring
+else:
+    print(f"'{substring_to_search} nowhere to be found in the string.'")
